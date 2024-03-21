9bff8cb6e04dcd05034f1592c88eedb92c61804c
KekmaTime <22am014@sctce.ac.in>
2024-03-21 05:47:44 +0530

    style: Refactor UI components for improved navigation and user experience
    - Wrapped Logo component in Link for better navigation
    - Added image background to landing page for visual appeal
    - Enhanced landing page layout and styles for better user experience
    - Updated landing page content and CTA buttons

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,41 +1,51 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { faCoins } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { Logo } from '../Logo/Logo';
+import Image from "next/image";
+import Link from "next/link";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { Logo } from "../Logo/Logo";
 
 export const AppLayout = ({ children }) => {
-  const {user } = useUser();
+  const { user } = useUser();
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
         <div className="bg-zinc-800 px-2">
-          <Logo/>
-          <Link href='/post/new' className='text-zinc-100 bg-zinc-400 tracking-wider w-full font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-zinc-600 transition-colors block'>New Post</Link>
-          <Link href='/token-topup' className='block mt-2 text-center'>
-            <FontAwesomeIcon icon={faCoins} className='fa-thin fa-coins text-zinc-400'/>
-            <span className='pl-1'>
-                0 Tokens available
-                </span>
-            </Link>
+          <Link href="/">
+            <Logo />
+          </Link>
+          <Link
+            href="/post/new"
+            className="text-zinc-100 bg-zinc-400 tracking-wider w-full font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-zinc-600 transition-colors block"
+          >
+            New Post
+          </Link>
+          <Link href="/token-topup" className="block mt-2 text-center">
+            <FontAwesomeIcon
+              icon={faCoins}
+              className="fa-thin fa-coins text-zinc-400"
+            />
+            <span className="pl-1">0 Tokens available</span>
+          </Link>
         </div>
         <div className="flex-1 overflow-auto bg-zinc-800">List of Posts</div>
         <div className="bg-zinc-800 flex item-center gap-2 border-t border-t-black/50 h-20 px-2 py-4">
           {user ? (
             <>
-              <div className='min-w-[50px]'>
+              <div className="min-w-[50px]">
                 <Image
                   src={user.picture}
                   alt={user.name}
                   width={50}
                   height={50}
-                  className='rounded-full'
+                  className="rounded-full"
                 />
               </div>
-              <div className='flex-1'>
-                <div className='font-bold'>{user.email}</div>
-                <Link className="text-sm" href="/api/auth/logout">Logout</Link>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
               </div>
             </>
           ) : (
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,45 @@
+import Image from "next/image";
+import { Logo } from "../components/Logo";
+import Scott from "../public/hero.webp";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user } = useUser();
   return (
-    <h1>
-      <div>
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={Scott} alt="LandingPage" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-zinc-600/40 rounded-md backdrop-blur-sm">
+        <Logo />
+        <p>
+          An OpenAI-Powered SAAS for SEO-Optimized Blogs. Effortlessly create
+          top-quality content in minutes, saving you time without compromising
+          on quality.
+        </p>
+        {user ? (
+          <>
+            <Link
+              href="/post/new"
+              className="bg-zinc-400 tracking-wider w-full font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-zinc-600 transition-colors block mb-4"
+            >
+              Start
+            </Link>
+            <Link
+              href="/api/auth/logout"
+              className="bg-red-400 tracking-wider w-full font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-red-600 transition-colors block"
+            >
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link
+            href="/post/new"
+            className="bg-zinc-400 tracking-wider w-full font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-zinc-600 transition-colors block"
+          >
+            Start
+          </Link>
+        )}
       </div>
-    </h1>
+    </div>
   );
 }
